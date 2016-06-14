51d39d832ba3b8cfddc25cac66aec42704cfa9a5
gotitmemorize <jpmemorize@ucla.edu>
2016-06-14 04:55:09 -0700

    Added to the ui a part that shows what gun is selected. Guns are now access via the scroll wheel
    Also made some other small changes. Made the enemies "jump" when moving
    up and down between the platform and ground to make it so they don't
    clip through the platform (done in a previous commit)

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -10,6 +10,8 @@ public class GunManager : MonoBehaviour {
 
 	bool swappingGuns = false;
 	public int current_gun = 0;
+    private float scroll_gun = 0;
+    private int next_gun = 0;
 	// Use this for initialization
 	void Start () {
 		hud = HUD.GetComponent<HUDManager> ();
@@ -24,13 +26,15 @@ public class GunManager : MonoBehaviour {
 	
 
 		bool swapGuns = Input.GetKeyDown("t");
-		if (swappingGuns == true)
+        scroll_gun = Input.GetAxis("Mouse ScrollWheel");
+
+        if (swappingGuns == true)
 		{
 			if (GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<FireGun> ().active_gun == true)
 				return;
 			
-			current_gun++;
-			current_gun = current_gun % GunsArray.Length;
+			current_gun += next_gun;
+			current_gun = current_gun - GunsArray.Length * ((int) Mathf.Floor(((float)current_gun)/ GunsArray.Length));
 			GunsArray [current_gun].transform.GetChild (0).gameObject.SetActive (true);
 			GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<Animator> ().SetTrigger ("SwapIn");
 			GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<FireGun>().active_gun = true;
@@ -39,10 +43,18 @@ public class GunManager : MonoBehaviour {
 			return;
 
 		}
-		if (swapGuns && GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Default")) {
+		if (scroll_gun != 0 && GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Default")) {
 			//GunsArray [current_gun].SetActive (false);
 			GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<FireGun> ().active_gun = false;
 			GunsArray [current_gun].transform.GetChild (0).gameObject.GetComponent<Animator> ().SetTrigger ("SwapOut");
+            if (scroll_gun > 0)
+            {
+                next_gun = -1;
+            }
+            else
+            {
+                next_gun = 1;
+            }
 			swappingGuns = true;
 
 
--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -7,7 +7,9 @@ public class HUDManager : MonoBehaviour {
 
 
     public GameObject gun;
+    public GameObject gun_manager;
     FireGun firegun;
+    GunManager gun_manager_script;
     public Text ammo_display;
     int ammo_count;
     public GameObject pauseMenu;
@@ -18,6 +20,13 @@ public class HUDManager : MonoBehaviour {
     bool gameover = false;
 	public Text health_value_display;
     Animator anim;
+    public Image damaged_image;
+    public Color damaged_color = new Color(1f, 0f, 0f, 0.1f);
+    public Color health_color = new Color(0f, 1f, 0f, 0.1f);
+    public Color selected_color = new Color(0f, 0f, 1f);
+    public Color not_selected_color = new Color(0f, 0f, 0.1f);
+    public float damaged_fade_speed = 3f;
+    public RawImage [] gun_selected;
 
 
     // Use this for initialization
@@ -26,6 +35,7 @@ public class HUDManager : MonoBehaviour {
         player = GameObject.FindGameObjectWithTag("Player");
         playerhealth = player.GetComponent<PlayerHealth>();
         anim = gameObject.GetComponent<Animator>();
+        gun_manager_script = gun_manager.GetComponent<GunManager>();
 
 
     }
@@ -36,6 +46,20 @@ public class HUDManager : MonoBehaviour {
         anim.speed = firegun.animation_speed;
         ammo_count = firegun.ammo_in_clip;
         ammo_display.text = ammo_count.ToString() + "/" + firegun.clip_size.ToString();
+        if (int.Parse(health_value_display.text) > playerhealth.Health)
+        //if (string.Compare(health_value_display.text,playerhealth.Health.ToString()) > 0)
+        {
+            damaged_image.color = damaged_color;
+        }
+        else if (int.Parse(health_value_display.text) < playerhealth.Health)
+        {
+            damaged_image.color = health_color;
+        }
+        else
+        {
+            
+            damaged_image.color = Color.Lerp(damaged_image.color, Color.clear, damaged_fade_speed * Time.deltaTime);
+        }
 		health_value_display.text = playerhealth.Health.ToString();
 
         if (playerhealth.Health <= 0)
@@ -45,6 +69,20 @@ public class HUDManager : MonoBehaviour {
             Time.timeScale = 0.0F;
         }
 
+
+
+        //display which gun we have
+        for (int i = 0; i < gun_selected.Length; i++)
+        {
+            if (i == gun_manager_script.current_gun)
+            {
+                gun_selected[i].color = selected_color;
+            }
+            else
+            {
+                gun_selected[i].color = not_selected_color;
+            }
+        }
 
 
     }
